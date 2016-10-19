16d15f95593c04c06937ec565bfd20c803ad5dca
eSilvac <esilva1995@gmail.com>
2016-10-19 11:50:51 -0500

    Se creo metodo post, y el boton de crear tarea

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,21 @@
 class ProductsController < ApplicationController
-  def index
-  	@products = Product.all
-  end
-  def new
-  	@products = Product.new
-  end
+  	def index
+  		@products = Product.all
+  	end
+    def new
+  		@products = Product.new
+    end
+    def create
+	  	@products = Product.new(product_params)
+	  	if @products.save
+	  		redirect_to products_path, notice: "Producto bien publicado"
+	  	else
+	  		render :new
+	  	end
+ 	end
+
+	private
+	  	def product_params
+	  		params.require(:product).permit(:name, :url, :description)
+	  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
 	get '/products' => 'products#index'
-	get '/products/new' => 'products#new'
+	get '/products/new' => 'products#new', as: 'new_product'
+	post '/products' => 'products#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
