d8dd19c6e954929853f6b7b87d15aa6be0dea100
eSilvac <esilva1995@gmail.com>
2016-10-25 10:41:14 -0500

    Grandes Avances

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,9 +2,11 @@ class ProductsController < ApplicationController
   	def index
   		@products = Product.all
   	end
+
     def new
-  		@products = Product.new
+  		@product = Product.new
     end
+
     def create
 	  	@products = Product.new(product_params)
 	  	if @products.save
@@ -12,6 +14,29 @@ class ProductsController < ApplicationController
 	  	else
 	  		render :new
 	  	end
+ 	end
+
+ 	def show
+ 		@product = Product.find(params[:id])
+ 	end
+
+ 	def edit
+ 		@product = Product.find(params[:id])
+ 	end
+
+ 	def update
+ 		@product = Product.find(params[:id])
+ 		if @product.update(product_params)
+ 			redirect_to products_path, notice: "Producto modificado"
+ 		else
+ 			render :edit
+ 		end
+ 	end
+
+ 	def destroy
+		product = Product.find(product_params)
+		product.destroy
+		render :index 		
  	end
 
 	private
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
 	get '/products' => 'products#index'
 	get '/products/new' => 'products#new', as: 'new_product'
 	post '/products' => 'products#create'
+	get	'/products/:id' =>'products#show', as: 'product'
+	get '/products/:id/edit' => 'products#edit', as:'edit_product'
+	patch '/products/:id' => 'products#update'
+	delete '/products/:id' => 'products#destroy' , as:'delete_product'
+	#post '/products' => 'products#deleate', as: 'delete_product'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
